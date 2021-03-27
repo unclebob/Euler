af3f5f697de5f4e607405287393c43cc250c628d
Robert C. Martin <unclebob@cleancoder.com>
2021-03-27 14:32:16 -0500

    Turn right command.

--- e2 Fibonacci/Turtle/src/turtle_graphics/turtle.clj
+++ e2 Fibonacci/Turtle/src/turtle_graphics/turtle.clj
@@ -6,6 +6,8 @@
    :heading 0
    :velocity 0
    :distance 0
+   :omega 0
+   :angle 0
    :state :idle})
 
 (def WIDTH 10)
@@ -36,30 +38,39 @@
   (assoc turtle :velocity velocity)
   )
 
-(defn update-turtle [{:keys [position velocity heading distance state] :as turtle}]
+(defn update-turtle [{:keys [position velocity heading distance omega state angle] :as turtle}]
   (let [step (min velocity distance)
         distance (- distance step)
-        state (if (zero? distance) :idle state)
-        velocity (if (zero? distance) 0 velocity)
+        state (if (and (zero? distance) (zero? angle)) :idle state)
         radians (q/radians heading)
         [x y] position
         vx (* step (Math/cos radians))
         vy (* step (Math/sin radians))
-        position [(+ x vx) (+ y vy)]]
+        position [(+ x vx) (+ y vy)]
+
+        angle-step (min omega angle)
+        angle (- angle angle-step)
+        heading (+ heading angle-step)]
     (assoc turtle :position position
+                  :heading heading
                   :distance distance
-                  :velocity velocity
+                  :velocity (if (zero? distance) 0 velocity)
+                  :angle angle
+                  :omega (if (zero? angle) 0 omega)
                   :state state))
   )
 
 (defn forward [turtle [distance]]
-  (assoc turtle :velocity 5 :distance distance :state :busy)
-  )
+  (assoc turtle :velocity 5 :distance distance :state :busy))
+
+(defn right [turtle [angle]]
+  (assoc turtle :omega 10 :angle angle))
 
 (defn handle-command [turtle [cmd & args :as command]]
   (prn command)
   (condp = cmd
     :forward (forward turtle args)
+    :right (right turtle args)
 
     :else turtle)
   )
