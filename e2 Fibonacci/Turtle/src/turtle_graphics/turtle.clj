164391026d0e83a97d5156f2fa50df21bb83c134
Robert C. Martin <unclebob@cleancoder.com>
2021-03-27 14:09:49 -0500

    forward without drawing.

--- e2 Fibonacci/Turtle/src/turtle_graphics/turtle.clj
+++ e2 Fibonacci/Turtle/src/turtle_graphics/turtle.clj
@@ -3,7 +3,9 @@
 
 (defn make []
   {:position [0 0]
-   :heading 0})
+   :heading 0
+   :velocity 0
+   :distance 0})
 
 (def WIDTH 10)
 (def HEIGHT 15)
@@ -33,19 +35,28 @@
   (assoc turtle :velocity velocity)
   )
 
-(defn update-turtle [{:keys [position velocity heading] :as turtle}]
-  (let [radians (q/radians heading)
+(defn update-turtle [{:keys [position velocity heading distance] :as turtle}]
+  (let [step (min velocity distance)
+        distance (- distance step)
+        velocity (if (zero? distance) 0 velocity)
+        radians (q/radians heading)
         [x y] position
-        vx (* velocity (Math/cos radians))
-        vy (* velocity (Math/sin radians))
+        vx (* step (Math/cos radians))
+        vy (* step (Math/sin radians))
         position [(+ x vx) (+ y vy)]]
-    (assoc turtle :position position))
+    (assoc turtle :position position
+                  :distance distance
+                  :velocity velocity))
+  )
+
+(defn forward [turtle [distance]]
+  (assoc turtle :velocity 5 :distance distance)
   )
 
 (defn handle-command [turtle [cmd & args :as command]]
   (prn command)
   (condp = cmd
-    :forward (assoc turtle :velocity 5)
+    :forward (forward turtle args)
 
     :else turtle)
   )
