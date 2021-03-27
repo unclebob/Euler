806d107cb7ca754f24d14b3f2d2f803c3b44a988
Robert C. Martin <unclebob@cleancoder.com>
2021-03-27 14:18:43 -0500

    Busy/Idle command sequencing

--- e2 Fibonacci/Turtle/src/turtle_graphics/turtle.clj
+++ e2 Fibonacci/Turtle/src/turtle_graphics/turtle.clj
@@ -5,7 +5,8 @@
   {:position [0 0]
    :heading 0
    :velocity 0
-   :distance 0})
+   :distance 0
+   :state :idle})
 
 (def WIDTH 10)
 (def HEIGHT 15)
@@ -35,9 +36,10 @@
   (assoc turtle :velocity velocity)
   )
 
-(defn update-turtle [{:keys [position velocity heading distance] :as turtle}]
+(defn update-turtle [{:keys [position velocity heading distance state] :as turtle}]
   (let [step (min velocity distance)
         distance (- distance step)
+        state (if (zero? distance) :idle state)
         velocity (if (zero? distance) 0 velocity)
         radians (q/radians heading)
         [x y] position
@@ -46,11 +48,12 @@
         position [(+ x vx) (+ y vy)]]
     (assoc turtle :position position
                   :distance distance
-                  :velocity velocity))
+                  :velocity velocity
+                  :state state))
   )
 
 (defn forward [turtle [distance]]
-  (assoc turtle :velocity 5 :distance distance)
+  (assoc turtle :velocity 5 :distance distance :state :busy)
   )
 
 (defn handle-command [turtle [cmd & args :as command]]
