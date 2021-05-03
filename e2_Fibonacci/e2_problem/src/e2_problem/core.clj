2c9eb3f9cf0b3d4b19d471e6a0315cd125891818
Robert C. Martin <unclebob@cleancoder.com>
2021-05-03 11:41:10 -0500

    memoize

--- e2_Fibonacci/e2_problem/src/e2_problem/core.clj
+++ e2_Fibonacci/e2_problem/src/e2_problem/core.clj
@@ -1,8 +1,17 @@
 (ns e2-problem.core
   (:require [clojure.tools.namespace.repl :refer [refresh]]))
 
-(defn fib [n]
-  (if (< n 2)
-    1
-    (+ (fib (- n 2))
-       (fib (- n 1)))))
+(def fib
+  (memoize
+    (fn [n]
+      (if (< n 2)
+        1
+        (+ (fib (- n 2))
+           (fib (- n 1)))))))
+
+(defn sum-even-fibs-upto [n]
+  (loop [i 0 fibs []]
+    (let [fib-i (fib i)]
+      (if (> fib-i n)
+        (reduce + (filter even? fibs))
+        (recur (inc i) (conj fibs fib-i))))))
