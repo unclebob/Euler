4c16d8bd62628fdc868eaadf10e94f728bcd0366
Robert C. Martin <unclebob@cleancoder.com>
2021-05-03 11:57:25 -0500

    Iteration

--- e2_Fibonacci/e2_problem/src/e2_problem/core.clj
+++ e2_Fibonacci/e2_problem/src/e2_problem/core.clj
@@ -1,13 +1,15 @@
 (ns e2-problem.core
   (:require [clojure.tools.namespace.repl :refer [refresh]]))
 
-(def fib
-  (memoize
-    (fn [n]
-      (if (< n 2)
-        1
-        (+ (fib (- n 2))
-           (fib (- n 1)))))))
+(defn fib
+  ([n]
+   (if (< n 2)
+     1
+     (fib (dec n) 1 1)))
+  ([n a b]
+   (if (zero? n)
+     b
+     (recur (dec n) b (+ a b)))))
 
 (defn sum-even-fibs-upto [n]
   (loop [i 0 fibs []]
