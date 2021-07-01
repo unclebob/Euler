dda83d8c5249ebad31be6e1d98e2631b2259ae65
Robert C. Martin <unclebob@cleancoder.com>
2021-07-01 10:42:25 -0500

    9 -> [3 3] good

--- e3_primeFactors/prime_factors/src/prime_factors/core.clj
+++ e3_primeFactors/prime_factors/src/prime_factors/core.clj
@@ -1,12 +1,11 @@
 (ns prime-factors.core)
 
 (defn factors-of [n]
-  (if (> n 1)
-    (cond
-      (= 0 (mod n 2))
-      (concat [2] (factors-of (quot n 2)))
-      (= 0 (mod n 3))
-      (concat [3] (factors-of (quot n 3)))
-      :else
-      [n])
-    []))
+  (loop [factors [] n n divisor 2]
+    (if (> n 1)
+      (if (= 0 (mod n divisor))
+        (recur (conj factors divisor)
+               (quot n divisor)
+               divisor)
+        (recur factors n (inc divisor)))
+      factors)))
