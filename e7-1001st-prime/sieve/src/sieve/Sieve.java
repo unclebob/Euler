0c79f9e9c015d901c12b36674ac1aaad1184e5e5
Robert C. Martin <unclebob@cleancoder.com>
2022-03-13 10:49:36 -0500

    Found the billionth prime.  Pretty fast too.

--- e7-1001st-prime/sieve/src/sieve/Sieve.java
+++ e7-1001st-prime/sieve/src/sieve/Sieve.java
@@ -12,31 +12,44 @@ public class Sieve {
     return sieve(n);
   }
 
-  private static List<Long> sieve(long n) {
+  public static List<Long> sieve(long n) {
     List<Long> primes = new ArrayList<>();
-    int nwords = (int)(n/32)+1;
+    int nwords = (int) (n / 32) + 1;
     composites = new int[nwords];
-    for (long candidate = 2; candidate <= n; candidate++) {
+    int count = 0;
+    int period = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 *23;
+    long multipleLimit = period*32L;
+    for (long candidate = 2; candidate <= n; candidate += (candidate == 2 ? 1 : 2)) {
       if (!isComposite(candidate)) {
-        primes.add(candidate);
-        for (long multiple = 2 * candidate; multiple <= n; multiple += candidate)
-          setComposite(multiple);
+        if (++count >= 999999999) {
+          System.out.println("n= " + count + " prime = " + candidate);
+        }
+        //primes.add(candidate);
+        if (count > 1000000001)
+          return primes;
+        for (long multiple = 2 * candidate; multiple <= n; multiple += candidate) {
+          if (multiple == multipleLimit) {
+            System.arraycopy(composites, 0, composites, period, nwords - period);
+            break;
+          } else
+            setComposite(multiple);
+        }
       }
     }
     return primes;
   }
 
   private static void setComposite(long candidate) {
-    int wordIndex = (int) (candidate/32);
-    int bitIndex = (int)(candidate%32);
+    int wordIndex = (int) (candidate / 32);
+    int bitIndex = (int) (candidate % 32);
     int bit = (1 << bitIndex);
     composites[wordIndex] |= bit;
   }
 
   private static boolean isComposite(long candidate) {
-    int wordIndex = (int) (candidate/32);
+    int wordIndex = (int) (candidate / 32);
     int theWord = composites[wordIndex];
-    int bitIndex = (int)(candidate%32);
+    int bitIndex = (int) (candidate % 32);
     int bit = (1 << bitIndex);
     int result = theWord & bit;
     return result != 0;
--- e7-1001st-prime/src/e7_1001st_prime/core.clj
+++ e7-1001st-prime/src/e7_1001st_prime/core.clj
@@ -1,6 +1,20 @@
 (ns e7-1001st-prime.core
   (:import (sieve Sieve)))
 
+(defn factors-of [n]
+  (loop [factors [] n n divisor 2]
+    (if (> n 1)
+      (cond
+        (> divisor (Math/sqrt n))
+        (conj factors n)
+        (= 0 (mod n divisor))
+        (recur (conj factors divisor)
+               (quot n divisor)
+               divisor)
+        :else
+        (recur factors n (inc divisor)))
+      factors)))
+
 (defn fast-primes-up-to [n]
   (vec (Sieve/primesUpTo n)))
 
