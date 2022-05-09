63023c9616f60457ada18210b80a57854b2da2a1
Robert C. Martin <unclebob@cleancoder.com>
2022-05-09 10:17:59 -0500

    Easy solution

---
+++ e8-largest-product-in-a-series/src/e8_largest_product_in_a_series/core.clj
@@ -0,0 +1,22 @@
+(ns e8-largest-product-in-a-series.core
+  (:require [clojure.string :as string]))
+
+(defn parse-input [text]
+  (let [lines (string/split-lines text)
+        lines (map #(.trim %) lines)]
+    (apply str lines)))
+
+(defn string->digits [s]
+  (map #(- (int %) (int \0)) s))
+
+(defn gather-groups [group-size items]
+  (loop [items items
+         groups []]
+    (if (> group-size (count items))
+      groups
+      (recur (rest items) (conj groups (take group-size items))))))
+
+(defn find-largest-product [n digits]
+  (let [groups (gather-groups n digits)
+        products (map #(reduce * %) groups)]
+    (apply max products)))
