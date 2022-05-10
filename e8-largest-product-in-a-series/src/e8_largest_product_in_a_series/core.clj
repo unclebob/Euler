b2ba3b0c582c71f6b9a72a1effa318a6e4761514
Robert C. Martin <unclebob@cleancoder.com>
2022-05-10 09:50:37 -0500

    Let's do some statistics.

--- e8-largest-product-in-a-series/src/e8_largest_product_in_a_series/core.clj
+++ e8-largest-product-in-a-series/src/e8_largest_product_in_a_series/core.clj
@@ -1,6 +1,20 @@
 (ns e8-largest-product-in-a-series.core
   (:require [clojure.string :as string]))
 
+(defn factors-of [n]
+  (loop [factors [] n n divisor 2]
+    (if (> n 1)
+      (cond
+        (> divisor (Math/sqrt n))
+        (conj factors n)
+        (= 0 (mod n divisor))
+        (recur (conj factors divisor)
+               (quot n divisor)
+               divisor)
+        :else
+        (recur factors n (inc divisor)))
+      factors)))
+
 (defn parse-input [text]
   (let [lines (string/split-lines text)
         lines (map #(.trim %) lines)]
@@ -16,43 +30,16 @@
       groups
       (recur (rest items) (conj groups (take group-size items))))))
 
-;(defn find-largest-product [n digits]
-;  (let [groups (gather-groups n digits)
-;        groups (filter #(not-any? zero? %) groups)]
-;    (if (empty? groups)
-;      [0 []]
-;      (let [products (map #(reduce *' %) groups)
-;            pairs (apply hash-map (interleave products groups))
-;            max-product (apply max products)
-;            group (get pairs max-product)]
-;        [max-product group]))))
-
 (defn find-largest-product [n digits]
-  (loop [digits digits
-         p (reduce *' (take n digits))
-         pmax 0
-         group (take n digits)]
-    (let [
-          pmax (max pmax p)
-          group (if (= p pmax) (take n digits) group)
-          first-digit (first digits)
-          rest-digits (doall (rest digits))
-          next-n (doall (take n rest-digits))]
-      (cond
-        (>= n (count digits))
-        [pmax group]
-
-        (zero? first-digit)
-        (recur rest-digits
-               (reduce *' next-n)
-               pmax
-               group)
-
-        :else
-        (recur rest-digits
-               (/ (*' p (last next-n)) first-digit)
-               pmax
-               group)))))
+  (let [groups (gather-groups n digits)
+        groups (filter #(not-any? zero? %) groups)]
+    (if (empty? groups)
+      [0 []]
+      (let [products (map #(reduce *' %) groups)
+            pairs (apply hash-map (interleave products groups))
+            max-product (apply max products)
+            group (get pairs max-product)]
+        [max-product group]))))
 
 (defn find-longest-product [digits]
   (loop [n 1
